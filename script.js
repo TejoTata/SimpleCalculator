4f0a1e59dd576a73c048a5705953b7a208b4ae61
Tejo <111945716+TejoTata@users.noreply.github.com>
2024-06-05 12:26:20 -0500

    created btn attributes to select in the js and added js

---
+++ script.js
@@ -0,0 +1,9 @@
+const numberButtons= Document.querrySelectorAll('[data-number]')
+const operationButton= Document.querrySelectorAll('[data-operation]')
+const equalsButton= Document.querrySelect('[data-equals]')
+const deleteButton= Document.querrySelect('[data-delete]')
+const allClearButton= Document.querrySelect('[data-all-clear]')
+const previousOperandTextElement= Document.querrySelect('[data-previous-operand]')
+const currentOperandTextElement = Document.querrySelect('[data-current-operand]')
+
+
