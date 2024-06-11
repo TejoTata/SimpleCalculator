356212dfb9896358d24c7e93f73857b118343ca8
Tejo <111945716+TejoTata@users.noreply.github.com>
2024-06-11 16:16:49 -0500

    error

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class Calculator{
     constructor(previousOperandTextElement,currentOperandTextElement){
-        this.previousOperandTextElement=previousOperandTextElement
-        this.currentOperandTextElement=currentOperandTextElement
+        this.previousOperandTextElement= previousOperandTextElement
+        this.currentOperandTextElement= currentOperandTextElement
         this.clear()
     }
 
     clear(){
-        this.currentOperandTextElement= ''
-        this.previousOperandTextElement=''
+        this.currentOperand= ''
+        this.previousOperand=''
         this.operation= undefined
     }
 
@@ -35,18 +35,18 @@ class Calculator{
 
 
 
-const numberButtons= Document.querrySelectorAll('[data-number]')
-const operationButton= Document.querrySelectorAll('[data-operation]')
-const equalsButton= Document.querrySelect('[data-equals]')
-const deleteButton= Document.querrySelect('[data-delete]')
-const allClearButton= Document.querrySelect('[data-all-clear]')
-const previousOperandTextElement= Document.querrySelect('[data-previous-operand]')
-const currentOperandTextElement = Document.querrySelect('[data-current-operand]')
+const numberButtons= document.querySelectorAll('[data-number]')
+const operationButton= document.querySelectorAll('[data-operation]')
+const equalsButton= document.querySelector('[data-equals]')
+const deleteButton= document.querySelector('[data-delete]')
+const allClearButton= document.querySelector('[data-all-clear]')
+const previousOperandTextElement= document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator=new Calculator(previousOperandTextElement,currentOperandTextElement)
 
 numberButtons.forEach(button =>{
-    button.addEventListner('click',() => {
+    button.addEventListener('click',() => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
